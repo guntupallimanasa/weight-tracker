f86d5e0eb4bd17cefd5fde2dc3990b0c9efcdcee
manasa <manusankar410@gmail.com>
2022-01-05 12:37:38 +0530

    modified files

--- src/index.js
+++ src/index.js
@@ -5,27 +5,12 @@ import {createStore} from 'redux'
 import rootReducer from './Reducers'
 import App from "./App";
 import Routes from "./routes";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink
-} from '@apollo/client'
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri:"http://localhost:7002/graphql"
-  })
-})
 
 const store = createStore(rootReducer);
 
-render(<ApolloProvider client = {client}>
-  <Provider store = {store}>
+render(<Provider store = {store}>
   {/* <App /> */}
   <Routes />
-  </Provider>
-  </ApolloProvider>, document.getElementById("root"));
+  </Provider>, document.getElementById("root"));
 
 
